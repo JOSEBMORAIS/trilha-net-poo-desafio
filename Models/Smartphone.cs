033ea45615d30a5867745cde720c05973155b4bb
Jose Morais <jbmpel@hotmail.com>
2025-10-24 14:59:37 -0300

    Adiciona classes Smartphone e Nokia

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,11 +3,17 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
+        public string Modelo { get; set; }
+        public string Imei { get; set; }
+        public int Memoria { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
             // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
@@ -18,7 +24,7 @@ namespace DesafioPOO.Models
 
         public void ReceberLigacao()
         {
-            Console.WriteLine("Recebendo ligação...");
+            Console.WriteLine("Recebendo ligação..da Maria Jose");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia c1 = new Nokia("971193849", "Antiago", "no001", 5215);
+c1.Ligar();
+c1.ReceberLigacao();
+c1.InstalarAplicativo("Whatzap");
+
+Iphone c2 = new Iphone("971193845", "Recente", "no002", 5214);
+c1.Ligar();
+c2.ReceberLigacao();
+c2.InstalarAplicativo("Google");
